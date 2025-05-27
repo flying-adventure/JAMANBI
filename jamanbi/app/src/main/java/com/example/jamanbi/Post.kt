7efbec9fe17473a339098658be58b7d696ab1530
rkd928 <ind07316@naver.com>
2025-05-27 21:56:12 +0900

    post xml fix

--- jamanbi/app/src/main/java/com/example/jamanbi/Post.kt
+++ jamanbi/app/src/main/java/com/example/jamanbi/Post.kt
@@ -6,6 +6,6 @@ data class Post(
     var content: String = "",                  // 게시글 본문
     var timestamp: Long = 0L,                  // 작성 시각
     var likes: Int = 0,                        // 좋아요 수
-    var isExternal: Boolean = false,           // ✅ 외부(Naver) 글 여부 (기본값 false)
-    var externalUrl: String = ""               // ✅ 외부 블로그 링크 (기본값 "")
+    var isExternal: Boolean = false,           // 외부(Naver) 글 여부 (기본값 false)
+    var externalUrl: String = ""               //  외부 블로그 링크 (기본값 "")
 )
