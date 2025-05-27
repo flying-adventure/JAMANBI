e89854b9e274a0fca26384554ffc80568371c677
somettt <somethinaws@gmail.com>
2025-05-27 21:52:16 +0900

    add fragment

--- jamanbi/app/build.gradle.kts
+++ jamanbi/app/build.gradle.kts
@@ -84,4 +84,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
+
+    //fragment
+    implementation ("androidx.fragment:fragment-ktx:1.6.2")
 }
